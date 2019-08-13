03c5b13098439d4088989a29a5f28e5fc3ef0ea8
akmalwidad <widad.akmal83@gmail.com>
2019-08-13 09:18:28 +0700

    calculator

---
+++ minggu-4/class.js
@@ -0,0 +1,16 @@
+class OperasiHitung{
+    pertambahan(a,b){
+        return a+b
+    }
+    perkalian(a,b){
+        return a*b
+    }
+    pembagian(a,b){
+        return a/b
+    }
+    pengurangan(a,b){
+        return a-b
+    }
+}
+
+const OH = new OperasiHitung()
